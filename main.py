d4d7610b7f9bb582fd8496266145e40d839d71fb
ninja-NikhilChauhan <nikhil.chauhan@codingninjas.com>
2025-05-10 17:12:42 +0000

    some changes

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://tafrontendnext.vercel.app/"], 
+    allow_origins=["https://tafrontendnext.vercel.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
